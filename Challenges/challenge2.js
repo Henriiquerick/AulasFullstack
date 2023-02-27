788a1f723c912ff097397817e6e03f7c339c9c04
Rickson Henrique (Xucacodev) <88748126+Henriiquerick@users.noreply.github.com>
2023-02-26 22:00:59 -0300

    add challenge 2

---
+++ Challenges/challenge2.js
@@ -0,0 +1,9 @@
+const helloName = (name) => {
+    alert("olá " + document.getElementById('name').value);
+}
+
+const helloNamee = (name) => {
+    console.log('Olá' + name);
+}
+
+helloNamee('Leozin');
