8452446e6a42e9125ffed207f66b10e284d8d6da
Rickson Henrique (Xucacodev) <88748126+Henriiquerick@users.noreply.github.com>
2023-02-26 22:54:35 -0300

    add else in challenge 5

--- Challenges/challenge5.js
+++ Challenges/challenge5.js
@@ -1,8 +1,9 @@
 function ifNull(originalObject){
-
     for(i in originalObject){
         if(originalObject[i] == null){
            delete originalObject[i];
+        } else{
+            console.log('Nada para deletar');
         }
     }
     console.log(originalObject)
